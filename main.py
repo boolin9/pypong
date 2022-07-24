bee7a08472e7fbf804fc0e843a1a88a4a8ab795a
andrewhought <80929614+boolin9@users.noreply.github.com>
2022-07-24 13:16:22 -0700

    Created screen and paddles with controls

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
 # Imports
+from turtle import Turtle, Screen
+from paddle import Paddle
 
 # Screen
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor('black')
+screen.title("PyPong")
+screen.tracer(0)
+
 
 # Objects
-## Paddle, ball, scoreboard
+r_paddle = Paddle((360, 0))
+l_paddle = Paddle((-360, 0))
+# ball = Ball()
+# scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(r_paddle.up, "Up")
+screen.onkey(r_paddle.down, "Down")
+
+screen.onkey(l_paddle.up, "w")
+screen.onkey(l_paddle.down, "s")
+
 
 # Game initializer
+game_on = True
 
 # Run game
+while game_on:
+    screen.update()
 ## Collisions
+
+
+screen.exitonclick()
--- paddle.py
+++ paddle.py
@@ -1,6 +1,27 @@
 # Imports
+from turtle import Turtle, width
+
 
 # Paddle class
-## Initialize paddle
-
-# Paddle functions
+class Paddle(Turtle):
+    
+    
+    # Initialize paddle
+    def __init__(self, position):
+        super().__init__()
+        self.shape('square')
+        self.color('white')
+        self.shapesize(5, 1)
+        self.pu()
+        self.goto(position)
+        
+        
+    # Paddle functions
+    def up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+                
+                
+    def down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
